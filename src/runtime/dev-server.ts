cac65a4ad347dc5b5c18d72dd18905a729460f31
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-31 21:13:01 -0500

    style: js-beautify formatting (src/runtime/dev-server.ts)

--- src/runtime/dev-server.ts
+++ src/runtime/dev-server.ts
@@ -1,12 +1,14 @@
-import { writeAllSync } from 'https://deno.land/std@0.130.0/streams/conversion.ts';
+import {
+  writeAllSync
+} from 'https://deno.land/std@0.130.0/streams/conversion.ts';
 import type {
-	Handler,
-	ConnInfo,
+  Handler,
+  ConnInfo,
 } from 'https://deno.land/std@0.177.0/http/server.ts';
 
 // deno-lint-ignore no-explicit-any
 function isNetAddr(v: any): v is Deno.NetAddr {
-	return v && typeof v.port === 'number';
+  return v && typeof v.port === 'number';
 }
 
 // Load the entrypoint handler function
@@ -16,44 +18,51 @@ Deno.env.delete('VERCEL_DEV_ENTRYPOINT');
 const mod = await import(`file://${entrypoint}`);
 const handler: Handler = mod.default;
 if (typeof handler !== 'function') {
-	throw new Error('Failed to load handler function');
+  throw new Error('Failed to load handler function');
 }
 
 // Spawn HTTP server on ephemeral port
-const listener = Deno.listen({ port: 0 });
+const listener = Deno.listen({
+  port: 0
+});
 
 if (isNetAddr(listener.addr)) {
-	const { port } = listener.addr;
-	const portBytes = new TextEncoder().encode(String(port));
+  const {
+    port
+  } = listener.addr;
+  const portBytes = new TextEncoder().encode(String(port));
 
-	try {
-		// Write the port number to FD 3
-		const portFd = Deno.openSync('/dev/fd/3', { read: false, write: true });
-		writeAllSync(portFd, portBytes);
-		Deno.close(portFd.rid);
-	} catch (_err) {
-		// This fallback is necessary for Windows
-		// See: https://github.com/denoland/deno/issues/6305
-		const portFile = Deno.env.get('VERCEL_DEV_PORT_FILE');
-		if (portFile) {
-			await Deno.writeFile(portFile, portBytes);
-		}
-	} finally {
-		Deno.env.delete('VERCEL_DEV_PORT_FILE');
-	}
+  try {
+    // Write the port number to FD 3
+    const portFd = Deno.openSync('/dev/fd/3', {
+      read: false,
+      write: true
+    });
+    writeAllSync(portFd, portBytes);
+    Deno.close(portFd.rid);
+  } catch (_err) {
+    // This fallback is necessary for Windows
+    // See: https://github.com/denoland/deno/issues/6305
+    const portFile = Deno.env.get('VERCEL_DEV_PORT_FILE');
+    if (portFile) {
+      await Deno.writeFile(portFile, portBytes);
+    }
+  } finally {
+    Deno.env.delete('VERCEL_DEV_PORT_FILE');
+  }
 }
 
 // Serve HTTP requests to handler function
 const conn = await listener.accept();
 const s = Deno.serveHttp(conn);
 for await (const req of s) {
-	const connInfo: ConnInfo = {
-		// @ts-ignore - `rid` is not on the `ConnInfo` interface, but it's required by Oak
-		rid: conn.rid,
-		localAddr: conn.localAddr,
-		remoteAddr: conn.remoteAddr,
-	};
-	Promise.resolve(handler(req.request, connInfo)).then((res: Response) =>
-		req.respondWith(res)
-	);
+  const connInfo: ConnInfo = {
+    // @ts-ignore - `rid` is not on the `ConnInfo` interface, but it's required by Oak
+    rid: conn.rid,
+    localAddr: conn.localAddr,
+    remoteAddr: conn.remoteAddr,
+  };
+  Promise.resolve(handler(req.request, connInfo)).then((res: Response) =>
+    req.respondWith(res)
+  );
 }
