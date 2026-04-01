061f17c6c9dbeb3375ab40733afd699033db6562
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-31 21:12:45 -0500

    style: js-beautify formatting (api/oak.ts)

--- api/oak.ts
+++ api/oak.ts
@@ -1,9 +1,11 @@
-import { Application } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+import {
+  Application
+} from 'https://deno.land/x/oak@v11.1.0/mod.ts';
 
 const app = new Application();
 
 app.use((ctx) => {
-	ctx.response.body = 'Hello World from Oak!';
+  ctx.response.body = 'Hello World from Oak!';
 });
 
 export default app.handle;
