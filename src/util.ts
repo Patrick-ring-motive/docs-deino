c2386c5b8b5b22625d07269055f8d4bdec414d46
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-31 21:13:12 -0500

    style: js-beautify formatting (src/util.ts)

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,12 @@
-import { URL } from 'url';
+import {
+  URL
+} from 'url';
 
 export function isURL(v: string) {
-	try {
-		new URL(v);
-		return true;
-	} catch {
-		return false;
-	}
+  try {
+    new URL(v);
+    return true;
+  } catch {
+    return false;
+  }
 }
